0da5c044af6e3fb57008c6276d3853cf07a8a30d
Vero Juarez <veronicajuarez01@hotmail.com>
2022-02-03 12:24:10 -0300

    Obtengo todos desde el backend

--- ensolvers-front/src/pages/EditToDo.jsx
+++ ensolvers-front/src/pages/EditToDo.jsx
@@ -1,11 +1,23 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { useParams } from "react-router-dom"
 
 export const EditToDo = () => {
   const { id } = useParams()
+  const [todo, setTodo] = useState({})
+
+  const fetchTodo = async () => {
+    const response = await axios.get(`http://localhost:4000/todo/${id}`)
+    const item = response.data
+    setTodo(item)
+  }
+
+  useEffect(() => fetchTodo(), [])
+
   return(
     <>
-    <h1>Editing task</h1>
-    <input type="text" defaultValue={id} />
+      <h1>Editing task</h1>
+      <input type="text" defaultValue={todo.title} />
     </>
   )
 }
--- ensolvers-front/src/pages/Home.jsx
+++ ensolvers-front/src/pages/Home.jsx
@@ -1,12 +1,25 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { ToDoItem } from '../components/ToDoItem'
 
 export const Home = () => {
+  const [todos, setTodos] = useState([])
+
+  const fetchTodos = async () => {
+    const response = await axios.get('http://localhost:4000/todo')
+    const items = response.data
+    setTodos(items)
+  }
+
+  useEffect(() => fetchTodos(), [])
+
   return (
     <>
       <h1>To-Do List</h1>
       <section>
-        <ToDoItem id={1} isCompleted title="Implementar abm de To Dos" />
-        <ToDoItem id={2} title="Implementar carpetas" />
+        { todos.map(todo => 
+          <ToDoItem id={todo.id} title={todo.title} isCompleted={todo.isCompleted} key={todo.id} />
+        )}
       </section>
     </>
   )
