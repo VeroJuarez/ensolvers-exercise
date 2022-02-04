452ff0517dee6300408e25221daee161102800e6
Vero Juarez <veronicajuarez01@hotmail.com>
2022-02-03 21:58:51 -0300

    Incluyo conexión con la base de datos

--- ensolvers-back/index.js
+++ ensolvers-back/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+const db = require('./src/db')
 
 const PORT = process.env.PORT || 4000
 
@@ -16,5 +17,14 @@ app.use(bodyParser.json())
 app.listen(PORT, () => {
 	console.log(`App corriendo en puerto ${PORT}.`)
 })
+
+app.get('/init', (req, res) => {
+  const sqlQuery =  'CREATE TABLE IF NOT EXISTS emails(id int AUTO_INCREMENT, firstname VARCHAR(50), lastname VARCHAR(50), email VARCHAR(50), PRIMARY KEY(id))';
+
+  db.query(sqlQuery, (err) => {
+      if (err) throw err;
+      res.send('Tabla de To-Dos creada')
+  });
+});
 
 app.use('/todo', toDoController)
