29fe915c1eae799e943b86836e393602557a86c7
Vero Juarez <veronicajuarez01@hotmail.com>
2022-02-09 14:00:53 -0300

    Instalo Tailwind para dar estilo

--- ensolvers-front/src/App.jsx
+++ ensolvers-front/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
 import { useSelector } from 'react-redux'
 import { getIsLoading } from './store/selectors/loading'
 import { Loading } from './components/Loading'
